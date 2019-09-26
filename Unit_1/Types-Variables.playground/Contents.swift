2f531ad8eed5c7a28ebf1f41159e4b5449765292
Gregory Keeley <gregkeeley@Gregorys-MacBook-Pro.local>
2019-09-26 10:43:07 -0400

    Added variables playground

---
+++ Unit_1/Types-Variables.playground/Contents.swift
@@ -0,0 +1,59 @@
+import UIKit
+
+var str = "Hello, playground"
+
+str
+
+str
+
+var str2 = "Don't go to the playground"
+
+var fellowName = "Eric"
+
+//OK this is a comment about code
+
+var cohort: String = "6.3 iOS"
+
+var alphabet: Character = "c"
+
+cohort = "7.0"
+
+cohort = "iOS 7.3"
+
+var age = 32
+
+var futureAge = age + 8
+
+print("Age of fellow 8 years from now is \(futureAge)")
+
+var balance = 1.5
+
+balance = balance + 1.5
+
+print (balance)
+
+var result = 70 < 100
+
+var otherResult = 200 > 300
+
+var thirdResult = result && true
+
+thirdResult && otherResult
+
+var salary = 20000
+
+salary *= 2
+
+print (salary)
+
+var modulo = 16%6
+
+print (modulo)
+
+let numSeven = 4.0 + 1.2
+let numEight = 5.0 + 0.2
+let d = numSeven == numEight
+
+let numFive = 24%5
+let numSix = 24%5
+let c = numFive == numSix
